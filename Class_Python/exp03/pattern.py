71102ababeb41556bad6d618531535438622d137
aggarwalshardul <aggarwalshardul@gmail.com>
2026-02-12 10:08:07 +0530

    Add files via upload

---
+++ Class_Python/exp03/pattern.py
@@ -0,0 +1,19 @@
+n = 5
+
+for i in range(n):
+    
+    for j in range(1, n - i + 1):
+        print(j, end="")
+
+    # spaces before stars
+    print(" " * i, end="")
+
+    # stars
+    if i > 0:
+        print("* " * i, end="")
+
+    # right numbers
+    for j in range(n - i, 0, -1):
+        print(j, end="")
+
+    print()
